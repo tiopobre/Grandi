3554960891c40849548b41401bbeda68a4d8d602
German Rodriguez <germanr_g@hotmail.com>
2020-03-30 12:20:03 -0500

    g

--- components/Layaout/header.jsx
+++ components/Layaout/header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto">
                         <li className="nav-item active">
-                            <Link href="/"><a className="nav-link" >INICIO <span className="sr-only">(current)</span></a></Link>
+                            <Link href="/"><a className="nav-link" >INICIO. <span className="sr-only">(current)</span></a></Link>
                         </li>
                         <li className="nav-item">
                             <Link href="/Alimentos" ><a className="nav-link">ALIMENTOS</a></Link>
